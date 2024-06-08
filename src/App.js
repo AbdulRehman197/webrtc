6d9bda503a731ac6ba80b1a09cc606636f00fbf8
manibai19761 <manibai19761@gmail.com>
2024-06-08 09:03:06 +0000

    change permisstion

--- src/App.js
+++ src/App.js
@@ -255,9 +255,9 @@ const App = () => {
       options.mode = "readwrite";
     }
     // Check if permission was already granted. If so, return true.
-    if ((await fileHandle.queryPermission(options)) === "granted") {
-      return true;
-    }
+    // if ((await fileHandle.queryPermission(options)) === "granted") {
+    //   return true;
+    // }
     // Request permission. If the user grants permission, return true.
     if ((await fileHandle.requestPermission(options)) === "granted") {
       return true;
