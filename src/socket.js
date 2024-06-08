c90a620126bfddf6281dcbcb52a4f500e9a58a04
manibai19761 <manibai19761@gmail.com>
2024-06-08 08:50:32 +0000

    change single server setting

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://honored-glittery-bar.glitch.me';
+const URL = process.env.NODE_ENV === 'production' ? 'https://honored-glittery-bar.glitch.me' : 'https://honored-glittery-bar.glitch.me';
 
 export const socket = io(URL,{
     path: "/webrtc",
